905c30b7350ba93a7e0728e892ece9dd1c0856f8
Jana <128377430+jana423151@users.noreply.github.com>
2026-04-29 20:18:57 +0300

    Initial commit

---
+++ src/components/NoResultsDiv.jsx
@@ -0,0 +1,8 @@
+const NoResultsDiv = () => {
+    return <div className="no-results">
+        <h3 className="title">Something went wrong</h3>
+        <p className="message">We're unable to retrieve te weather details.</p>
+   </div>
+}
+
+export default NoResultsDiv;
